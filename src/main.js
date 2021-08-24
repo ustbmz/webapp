eba79036a6e974e93da09350a0d2671595565695
cnmz <cnmz@msn.com>
2021-08-25 00:10:08 +0800

    init webapp project

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { Cell } from 'mint-ui'
+
+Vue.component(Cell.name, Cell)
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
