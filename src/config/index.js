11692308697f520c8d73db91a07300b5dfabd185
cnmz <cnmz@msn.com>
2021-11-13 18:26:29 +0800

    use https

--- src/config/index.js
+++ src/config/index.js
@@ -3,11 +3,11 @@ const isDev = process.env.NODE_ENV === 'development'
 export default {
   baseUrl: {
     dev: 'http://localhost:3000',
-    pro: 'http://121.36.201.245:12005'
+    pro: 'https://api.ustbmz.com'
   },
   publicPath: [/^\/public/, /^\/login/],
   wsconfig: {
     url: isDev ? '127.0.0.1' : '121.36.201.245',
-    prot: isDev ? '3001' : '20001'
+    prot: isDev ? '12005' : '12005'
   }
 }
